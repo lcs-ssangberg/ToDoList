a04134a2fd3b6ed0ba76ef64dd5de2bedcccf7b2
Sebastian Sangberg <ssangberg@lcs.on.ca>
2025-05-05 11:33:29 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Sebastian on 2025-05-05.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
